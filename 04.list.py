a0aa77037d642a22f35275ffb25117da46373352
Алексей <aiex.0115970@gmail.com>
2020-09-14 12:52:46 +0500

    lesson_dates_and_mark

--- 04.list.py
+++ 04.list.py
@@ -5,7 +5,24 @@ lesson_dates = [
     '19.05.19',
     '19.05.22',
 ]
-student_marks = [5, 4, 3, 2, 5]
+student_marks = [
+    5,
+    4,
+    3,
+    2,
+    5
+]
 
-for i, mark in enumerate(student_marks):
-    print(lesson_dates[i], 'оценка', mark)
+lesson_dates_and_mark = [
+    ['19.05.15', 5],
+    ['19.05.17', 4],
+    ['19.05.18', 3],
+    ['19.05.19', 2],
+    ['19.05.22', 5],
+]
+
+# for i, mark in enumerate(student_marks):
+#     print(lesson_dates[i], 'оценка', mark)
+
+for lesson_dates, mark in lesson_dates_and_mark:
+    print(lesson_dates, 'оценка', mark)
