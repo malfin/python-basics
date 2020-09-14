2f85a8d3f2f348f34648e5a4405d4dddd47acd0c
Алексей <aiex.0115970@gmail.com>
2020-09-14 12:38:17 +0500

    in place

--- 04.list.py
+++ 04.list.py
@@ -7,20 +7,5 @@ lesson_dates = [
 ]
 student_marks = [5, 4, 3, 2, 5]
 
-# i = 0
-# while i < len(student_marks):
-#     print(lesson_dates[i], 'оценка', student_marks[i])
-#     i += 1  # i = 1
-#
-# for mark in student_marks: #mark
-#     print('оценка', mark)
-#
-# user_full_name = ['Ivan', 'Ivanov']
-# first_name = user_full_name[0]
-# second_name = user_full_name[1]
-#
-# # first_name, second_name = ['Ivan', 'Ivanov']
-# print(first_name, second_name)
-
 for i, mark in enumerate(student_marks):
     print(lesson_dates[i], 'оценка', mark)
