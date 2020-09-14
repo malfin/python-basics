8a381ce1a24c1af0d6c2ed40f36c84c68800477d
Алексей <aiex.0115970@gmail.com>
2020-09-14 11:04:13 +0500

    While True

--- 01.list.py
+++ 01.list.py
@@ -7,3 +7,15 @@ while True:
         break
 print('Ввод завершен')
 print(student_marks)
+# ['5', '4', '3', '2']
+#   0    1    2    3
+i = 0
+avg_mark = 0
+while i < len(student_marks):
+    # print(type(avg_mark), type(student_marks))
+    # avg_mark = avg_mark + int(student_marks[i])
+    avg_mark += int(student_marks[i])
+    # i = i + 1
+    i += 1
+avg_mark /= len(student_marks)
+print('Средний балл:', avg_mark)
