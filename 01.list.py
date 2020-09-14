9fee2c5876a666379e9360185016a93686ad9760
Алексей <aiex.0115970@gmail.com>
2020-09-14 10:27:56 +0500

    Add while

--- 01.list.py
+++ 01.list.py
@@ -7,14 +7,25 @@ import sys
 #       - методы: действия (edit, delete, create, input_data)
 
 # 'yan.ru' -> reference
-my_var = [12, '15', 95.32, 55, True]  # var -> referecnce
-print(my_var, type(my_var), id(my_var), sys.getsizeof(my_var))
+# my_var = [12, '15', 95.32, 55, True]  # var -> referecnce
+# print(my_var, type(my_var), id(my_var), sys.getsizeof(my_var))
+#
+# my_var = 12
+# print(my_var, type(my_var), id(my_var), sys.getsizeof(my_var))
+#
+# my_var = 12.5
+# print(my_var, type(my_var), id(my_var), sys.getsizeof(my_var))
+#
+# my_var = '12.5'
+# print(my_var, type(my_var), id(my_var), sys.getsizeof(my_var))
 
-my_var = 12
-print(my_var, type(my_var), id(my_var), sys.getsizeof(my_var))
+student_marks = []
 
-my_var = 12.5
-print(my_var, type(my_var), id(my_var), sys.getsizeof(my_var))
-
-my_var = '12.5'
-print(my_var, type(my_var), id(my_var), sys.getsizeof(my_var))
+while True:
+    mark = input('Введите оценку студента:\t')
+    if mark:  # '' == False, 0 == False, [] == False
+        student_marks.append(mark)
+    else:
+        break
+print('Ввод завершен')
+print(student_marks)
