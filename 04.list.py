9915fca3dd3423cc48643eeabe63c4bfcd4f43d3
Алексей <aiex.0115970@gmail.com>
2020-09-14 11:55:15 +0500

    two lists

---
+++ 04.list.py
@@ -0,0 +1,13 @@
+lesson_dates = [
+    '19.05.15',
+    '19.05.17',
+    '19.05.18',
+    '19.05.19',
+    '19.05.22',
+]
+student_marks = [5, 4, 3, 2, 5]
+
+i = 0
+while i < len(student_marks):
+    print(lesson_dates[i], 'оценка', student_marks[i])
+    i += 1
