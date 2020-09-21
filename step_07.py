55809cf72cc76dd6ee8abb0a30aa503b60868763
Алексей <aiex.0115970@gmail.com>
2020-09-21 12:28:44 +0500

    strip

--- step_07.py
+++ step_07.py
@@ -2,10 +2,12 @@
 # Полина, Антон, Аресний, Евгений, Алексей, Тимур
 # assertion - утверждение
 # TDD -> test driven development
-class_pupils = 'Полина, Антон, Аресний, Евгений, Алексей, Тимур'
+class_pupils = 'Полина, Антон, Аресний , Евгений, Алексей, Тимур'
 correct_result = ['Полина', 'Антон', 'Аресний', 'Евгений', 'Алексей', 'Тимур']
 # print('учение класса:', class_pupils)
-result = class_pupils.split(', ')
-
+_result = class_pupils.split(', ')
+result = []
+for item in _result:
+    result.append(item.strip())
 assert result == correct_result, 'алгоритм реализован неверно'
 print(result)
