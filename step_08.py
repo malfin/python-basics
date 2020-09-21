3aa8849cc0bf3049c26bc162cfbfdfb531c422d9
Алексей <aiex.0115970@gmail.com>
2020-09-21 12:47:14 +0500

    after refactor

--- step_08.py
+++ step_08.py
@@ -3,9 +3,12 @@
 class_pupils = 'Полина,Антон, Аресний , Евгений, Алексей, Тимур'
 correct_result = ['Полина', 'Антон', 'Аресний', 'Евгений', 'Алексей', 'Тимур']
 
-_result = class_pupils.split(',')
-result = []
-for item in _result:
-    result.append(item.strip())
+# _result = class_pupils.split(',')
+# result = []
+# for item in _result:
+#     result.append(item.strip())
 
+# result = list(map(str.strip, class_pupils.split(',')))
+result = [item.strip().upper() for item in class_pupils.split(',')]
+print(result)
 assert result == correct_result, 'алгоритм реализован неверно'
