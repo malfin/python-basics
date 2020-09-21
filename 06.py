11cd355d01a9592107baf657eef7950d482ef8ef
Алексей <aiex.0115970@gmail.com>
2020-09-21 11:01:40 +0500

    Strings

--- 06.py
+++ 06.py
@@ -6,14 +6,17 @@ while True:
     if len(mark) != 1:
         print('Вы ввели не один символ!')
         continue
-    if not 48 <= ord(mark) <= 57:
-        print('вы ввели не число!')
-        continue
-    mark = int(mark)
-    if 1 <= mark <= 5:
-        student_marks.append(mark)
-    elif mark > 5:
-        print('Оценка больше 5!')
-    elif mark < 1:
-        print('Оценка меньше 1')
+    # if not 48 <= ord(mark) <= 57:
+    #     print('вы ввели не число!')
+    #     continue
+    try:
+        mark = int(mark)
+        if 1 <= mark <= 5:
+            student_marks.append(mark)
+        elif mark > 5:
+            print('Оценка больше 5!')
+        elif mark < 1:
+            print('Оценка меньше 1')
+    except Exception as e:
+        print('была допущена ошибка:', e)
 print('ввод завершен', student_marks)
