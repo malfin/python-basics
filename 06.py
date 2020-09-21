d0b1827a722baaf9d569163c847a59546965d7a5
Алексей <aiex.0115970@gmail.com>
2020-09-21 10:31:42 +0500

    Strings + Home Work

---
+++ 06.py
@@ -0,0 +1,19 @@
+student_marks = []
+while True:
+    mark = input('введите оценку студента\n')
+    if not mark:
+        break
+    if len(mark) != 1:
+        print('Вы ввели не один символ!')
+        continue
+    if not 48 <= ord(mark) <= 57:
+        print('вы ввели не число!')
+        continue
+    mark = int(mark)
+    if 1 <= mark <= 5:
+        student_marks.append(mark)
+    elif mark > 5:
+        print('Оценка больше 5!')
+    elif mark < 1:
+        print('Оценка меньше 1')
+print('ввод завершен', student_marks)
