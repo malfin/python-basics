050f45154fd28a2e2816cb51c34a446ce86f5bab
Алексей <aiex.0115970@gmail.com>
2020-09-21 12:34:21 +0500

    pre refactor

--- step_08.py
+++ step_08.py
@@ -1,15 +1,11 @@
 # class_pupils = input('введите имена учеников\n')
-# Полина, Антон, Аресний, Евгений, Алексей, Тимур
-# assertion - утверждение
-# TDD -> test driven development
 
 class_pupils = 'Полина,Антон, Аресний , Евгений, Алексей, Тимур'
 correct_result = ['Полина', 'Антон', 'Аресний', 'Евгений', 'Алексей', 'Тимур']
-# print('учение класса:', class_pupils)
+
 _result = class_pupils.split(',')
 result = []
 for item in _result:
     result.append(item.strip())
 
 assert result == correct_result, 'алгоритм реализован неверно'
-print(result)
