dddcdc3529f4663f6740648e0309591d10d3bc83
Алексей <aiex.0115970@gmail.com>
2020-09-27 21:42:27 +0500

    Home_WORK_UPDATE

--- home_work_2709.py
+++ home_work_2709.py
@@ -1,8 +1,8 @@
-row_record = '217.168.17.5 - - [17/May/2015:08:05:12 +0000] "GET /downloads/product_2 HTTP/1.1" 200 3316 "-" "-"'
+row_record = '217.168.17.5 - - [ 17/May/2015:08:05:12 +0000] "GET /downloads/product_2 HTTP/1.1" 200 3316 "-" "-"'
 
 correct_request_IP_address = '217.168.17.5'
 correct_request_data_time = '17/May/2015:08:05:12'
 
 request_IP_address = row_record.split()[0]
-request_data_time = row_record.split()[3]
+request_data_time = row_record.split()[4]
 print(request_IP_address, ',', request_data_time)
