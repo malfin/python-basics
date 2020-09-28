58af5e573dd9661fd04eb6805ec8ad6e3e86688a
Алексей <aiex.0115970@gmail.com>
2020-09-28 11:31:15 +0500

    with

--- 200928/03.py
+++ 200928/03.py
@@ -1,16 +1,10 @@
-f = open('nginx_logs_head.txt', 'r', encoding='utf-8')
+# f = open('nginx_logs_head.txt', 'r', encoding='utf-8')
+# context manager
 
-# file_data =
-# print(file_data)
-for row in f.read().splitlines():
-    remote_IP_address, row_tail = row.split(maxsplit=1)
-    _, _request_datetime = row_tail.split('[', maxsplit=1)
-    request_datetime, row_tail = _request_datetime.split(']', maxsplit=1)
-
-    _, _request_method, row_tail = row_tail.split('"', maxsplit=2)
-    request_method, requested_resource, _ = _request_method.split(maxsplit=2)
-
-    parsed_row = [remote_IP_address, request_datetime, request_method, requested_resource]
-    parsed_row = list(map(str.strip, parsed_row))
-    print(parsed_row)
-f.close()
+# __enter__
+# __exit__ -> f.close()
+# transaction.atomic() -> restore point
+with open('nginx_logs_head.txt', 'r', encoding='utf-8') as f:
+    for row in f.read().splitlines():
+        print(row)
+# print(f.closed)
