c7bd9c0cb8822c8f3e8841cf2b5e46d8be81e25f
Алексей <aiex.0115970@gmail.com>
2020-10-04 22:53:30 +0500

    Home WORK)

--- 200928/07.py
+++ 200928/07.py
@@ -1,6 +1,6 @@
 # f = open('nginx_logs_head.txt', 'r', encoding='utf-8')
 # context manager
-from log_parse import row_parse, save_parsed_data
+from log_parse import row_parse, parser
 
 parsed_data = row_parse('nginx_logs_head.txt')
 save_parsed_data('nginx_logs_parsed.csv', parsed_data)
---
+++ 200928/home_work.py
@@ -0,0 +1,22 @@
+from statistics import mean
+
+with open("student_log's.txt", 'r', encoding='utf-8') as f:
+    for list in f.read().splitlines():
+        l = len(list)
+        s_list = []
+        i = 0
+        while i < l:
+            s_int = ''
+            a = list[i]
+            while '0' <= a <= '9':
+                s_int += a
+                i += 1
+                if i < l:
+                    a = list[i]
+                else:
+                    break
+            i += 1
+            if s_int != '':
+                s_list.append(int(s_int))
+        print(list.split()[0], list.split()[1], list.split()[2].replace(",", ":"), s_list)
+        print('Средний балл:', mean(s_list))
