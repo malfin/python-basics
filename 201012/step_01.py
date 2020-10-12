3fc283fcca7104d427acba3f4ff576acf62e9ec4
Алексей <aiex.0115970@gmail.com>
2020-10-12 09:41:24 +0500

    201012 lession 1        - pop

--- 201012/step_01.py
+++ 201012/step_01.py
@@ -1,4 +1,9 @@
+# range(n) -> 0, 1, 2, n-1, [0,n)
 numbers = []
 for num in range(10):
     numbers.append(num)
 print(numbers)
+# [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+last_num = numbers.pop()  # like cut in word
+print(numbers)
+print(last_num)
